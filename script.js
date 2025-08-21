787b6334f9a762e370ffd9b0eaa572126718b9df
AnkurSainiAns <an2811saini@gmail.com>
2025-08-21 17:21:54 +0530

    new update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let Game = ["🥷", "🧟‍♂️", "🕷️", "🦂", "🦉", "🐍", "🐘", "🐊", "🦫", "🦔"]
+let Game = [`<img src="img/bhediya.png" alt="bhediya" width="100px">`,`<img src="img/bichu.png" alt="bichu" width="100px">`,`<img src="img/bull.png" alt="bull" width="100px">`,`<img src="img/chuso.png" alt="chuso" width="100px">`,`<img src="img/gorila.png" alt="gorila" width="100px">`,`<img src="img/hathi.png" alt="hathi" width="100px">`,`<img src="img/jaau.png" alt="jaau" width="100px">`,`<img src="img/kil.png" alt="kil" width="100px">`,`<img src="img/ki.png" alt="ki" width="100px">`,`<img src="img/kilkat.png" alt="kilkat" width="100px">`,`<img src="img/killkat.png" alt="killkat" width="100px">`,`<img src="img/lakad.png" alt="lakad" width="100px">`,`<img src="img/octo.png" alt="octo" width="100px">`,`<img src="img/sap.png" alt="sap" width="100px">`]
 let spaceEl = document.getElementById("space")
 let startGameEl = document.getElementById("StartGame")
 let fightEl = document.getElementById("Fight")
@@ -14,13 +14,13 @@ fightEl.addEventListener("click", function () {
     let randomIndex = Math.floor(Math.random() * Game.length)
     let randomIndex2 = Math.floor(Math.random() * Game.length)
 
-    spaceEl.textContent = Game[randomIndex] + "V/S" + Game[randomIndex2]
+    spaceEl.innerHTML = Game[randomIndex] + "V/S" + Game[randomIndex2]
     if (randomIndex === randomIndex2) {
-        resultEl.textContent = "it's a tie"
-    } else if (randomIndex > randomIndex2) {
-        resultEl.textContent = Game[randomIndex] + " wins"
+        resultEl.innerHTML = "it's a tie"
+    } else if (randomIndex < randomIndex2) {
+        resultEl.innerHTML = Game[randomIndex] + " wins"
     } else {
-        resultEl.textContent = Game[randomIndex2] + " wins"
+        resultEl.innerHTML = Game[randomIndex2] + " wins"
     }
 })
 resetEl.addEventListener("click", function () {
