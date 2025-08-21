c134f9927d5fa826e48c6f930465ae49c1c67623
AnkurSainiAns <an2811saini@gmail.com>
2025-08-21 17:58:58 +0530

    new update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let Game = [`<img src="img/bhediya.png" alt="bhediya" width="100px">`,`<img src="img/bichu.png" alt="bichu" width="100px">`,`<img src="img/bull.png" alt="bull" width="100px">`,`<img src="img/chuso.png" alt="chuso" width="100px">`,`<img src="img/gorila.png" alt="gorila" width="100px">`,`<img src="img/hathi.png" alt="hathi" width="100px">`,`<img src="img/jaau.png" alt="jaau" width="100px">`,`<img src="img/kil.png" alt="kil" width="100px">`,`<img src="img/ki.png" alt="ki" width="100px">`,`<img src="img/kilkat.png" alt="kilkat" width="100px">`,`<img src="img/killkat.png" alt="killkat" width="100px">`,`<img src="img/lakad.png" alt="lakad" width="100px">`,`<img src="img/octo.png" alt="octo" width="100px">`,`<img src="img/sap.png" alt="sap" width="100px">`]
+let Game = [`<img src="img/bhediya.png" alt="bhediya" width="100px">`,`<img src="img/bichu.png" alt="bichu" width="100px">`,`<img src="img/bull.png" alt="bull" width="100px">`,`<img src="img/chuso.png" alt="chuso" width="100px">`,`<img src="img/gorila.png" alt="gorila" width="100px">`,`<img src="img/jaau.png" alt="jaau" width="100px">`,`<img src="img/hathi.png" alt="hathi" width="100px">`,`<img src="img/kil.png" alt="kil" width="100px">`,`<img src="img/ki.png" alt="ki" width="100px">`,`<img src="img/kilkat.png" alt="kilkat" width="100px">`,`<img src="img/killkat.png" alt="killkat" width="100px">`,`<img src="img/lakad.png" alt="lakad" width="100px">`,`<img src="img/octo.png" alt="octo" width="100px">`,`<img src="img/sap.png" alt="sap" width="100px">`,`<img src="img/rino.png" alt="rino" width="100px">`
+,`<img src="img/ban.png" alt="ban" width="100px">`,`<img src="img/g.png" alt="g" width="100px">`,`<img src="img/goda.png" alt="goda" width="100px">`,`<img src="img/h.png" alt="h" width="100px">`,`<img src="img/hi.png" alt="hi" width="100px">`,`<img src="img/m.png" alt="m" width="100px">`
+,`<img src="img/mon.png" alt="mon" width="100px">`,`<img src="img/pata.png" alt="pata" width="100px">`,`<img src="img/un.png" alt="un" width="100px">`]
 let spaceEl = document.getElementById("space")
 let startGameEl = document.getElementById("StartGame")
 let fightEl = document.getElementById("Fight")
@@ -17,7 +19,7 @@ fightEl.addEventListener("click", function () {
     spaceEl.innerHTML = Game[randomIndex] + "V/S" + Game[randomIndex2]
     if (randomIndex === randomIndex2) {
         resultEl.innerHTML = "it's a tie"
-    } else if (randomIndex < randomIndex2) {
+    } else if (randomIndex > randomIndex2) {
         resultEl.innerHTML = Game[randomIndex] + " wins"
     } else {
         resultEl.innerHTML = Game[randomIndex2] + " wins"
